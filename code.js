2990aaf667d7906cb97f658ae4a63750338c842f
Howard Shaw <68131536+howardthegr8one@users.noreply.github.com>
2024-02-02 14:34:19 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,9 @@
 function fib(n) {
-    return n;
+    // Invariant: fibRecursion(n) = fibRecursion(n-1) + fibRecursion(n-2) 
+    if(n == 0) return [0];
+    if(n == 1) return [0,1];
+    
+    let arr = fib(n-1)
+    arr.push(arr[n-1] + arr[n-2]);
+    return arr;
 }
